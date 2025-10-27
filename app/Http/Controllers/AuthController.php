85dfcf2811808dce6a401fb4bdb806c8860f6700
swasthikaaa <swasthikalingaraj@example.com>
2025-10-27 18:01:35 +0530

    final

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -78,7 +78,7 @@ class AuthController extends Controller
 
         return response()->json([
             'success' => true,
-            'message' => '✅ Login successful',
+            'message' => 'Login successful',
             'user'    => [
                 'id'     => $user->id,
                 'name'   => $user->name,
