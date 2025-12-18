f818b7d98ad271238d59eb858cfc45bf558766c5
swasthikaaa <swasthikalingaraj@example.com>
2025-12-18 15:06:40 +0530

    Fix Vite build and simplify Vercel env

--- api/index.php
+++ api/index.php
@@ -1,10 +1,6 @@
 <?php
 
 // Set environment variables for Vercel serverless environment
-putenv('APP_CONFIG_CACHE=/tmp/config.php');
-putenv('APP_ROUTES_CACHE=/tmp/routes.php');
-putenv('APP_SERVICES_CACHE=/tmp/services.php');
-putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
 putenv('VIEW_COMPILED_PATH=/tmp');
 
 // Forward Vercel requests to normal index.php
