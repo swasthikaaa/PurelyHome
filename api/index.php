87829a9d075507e0a75fb244e2a24dc98fb74cda
swasthikaaa <swasthikalingaraj@example.com>
2025-12-18 15:03:13 +0530

    Final attempt to fix Vercel paths

--- api/index.php
+++ api/index.php
@@ -1,4 +1,11 @@
 <?php
 
+// Set environment variables for Vercel serverless environment
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('VIEW_COMPILED_PATH=/tmp');
+
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
